5647fa1e2d509a89cf55bbc8061ac628d2293cd4
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-04-27 17:52:02 +0800

    separate_numbers

--- p1_even_odd/object_oriented_odd_even.py
+++ p1_even_odd/object_oriented_odd_even.py
@@ -9,3 +9,20 @@ class number_processor:
         except FileNotFoundError:
             print("Input file not found.")
             return []
+
+    def separate_numbers(self, numbers):
+        even = []
+        odd = []
+
+        for num in numbers:
+            if num % 2 == 0:
+                even.append(num)
+            else:
+                odd.append(num)
+
+        return even, odd
+
+    def write_file(self, filename, data):
+        with open(filename, 'w') as file:
+            file.write(" ".join(map(str, data)))
+    
