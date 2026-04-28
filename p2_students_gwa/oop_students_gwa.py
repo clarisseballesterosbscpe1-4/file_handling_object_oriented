18eaa505e7b27830204ec2ef0a650d83615a9a88
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-04-28 12:32:06 +0800

    i fixed the top student and have a multiple top students on thelist

--- p2_students_gwa/oop_students_gwa.py
+++ p2_students_gwa/oop_students_gwa.py
@@ -8,12 +8,39 @@ class students_gwa:
             with open (self.students, 'r') as file:
                 for line in file:
                     name, gwa = line.split()
-                    students.append((name, float(gwa)))
+                    students.append((name, (gwa)))
         except FileNotFoundError:
             print("File not found")
         return students
-
     def find_student(self, students):
         if not students:
             return None
-        return min(students, key=lambda students: students[1])
+
+        best_gwa = students[0][1]
+
+        for student in students:
+            if student[1] == best_gwa:
+                best_gwa = student[1]
+
+        top_student = []
+        for student in students:
+            if student[1] == best_gwa:
+                top_student.append(student)
+        return top_student
+
+    def run(self):
+        students = self.read_students()
+        top = self.find_student(students)
+
+        top = self.find_student(students)
+
+        if top:
+            print("Top Students:")
+            for s in top:
+                print(s[0], "with GWA", s[1])
+        else:
+            print("No top student")
+
+if __name__ == "__main__":
+    app = students_gwa("students.txt")
+    app.run()
