83522d5f8000dadd956a0bba8f8aa3b977f8e061
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-04-29 03:41:02 +0800

    fixed my code 'til its working

--- p3_multiple_line/multiple line writer.py
+++ p3_multiple_line/multiple line writer.py
@@ -1,7 +1,21 @@
 class life_writer:
-    def __init__self(self,my_life):
+    def __init__(self, my_life):
         self.my_life = my_life
 
-    def write_line(self):
-        file = open("my_life.txt","w")
-        
+    def write_lines(self):
+        file = open(self.my_life, "w")
+
+        while True:
+            line = input("Enter your line: ")
+            file.write(line + "\n")
+
+            choice = input("Add more? (y/n): ")
+
+            if choice != "y":
+                break
+
+            file.close()
+            print("File saved.")
+
+app = life_writer("my_life.txt")
+app.write_lines()
