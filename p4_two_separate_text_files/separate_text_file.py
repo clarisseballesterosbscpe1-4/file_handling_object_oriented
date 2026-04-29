5f342c1086dd4acc97ff8c5262d0aec87f84d79a
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-04-29 16:26:58 +0800

    processing integers

--- p4_two_separate_text_files/separate_text_file.py
+++ p4_two_separate_text_files/separate_text_file.py
@@ -3,5 +3,54 @@ class NumberProcessor:
         self.integers = integers
 
     def read_integers(self):
-        with open("integers.txt", "r") as integers_file:
-            numbers =  integers_file.read().split()
+        try:
+            with open("integers.txt", "r") as integers_file:
+                content = integers.read().split()
+
+                numbers = []
+                for item in content:
+                    try:
+                        numbers.append(int(item))
+                    except ValueError:
+                        print(f"skipping invalid value: {item}")
+                return numbers
+        except FileNotFoundError:
+            print("input file not found")
+            return []
+    def separate_numbers(self, numbers):
+        even = []
+        odd = []
+
+        for num in numbers:
+            if num % 2 == 0:
+                even.append(num)
+            else:
+                odd.append(num)
+        return even, odd
+
+    def write_file(self, filename, data):
+        with open(filename, 'w') as file:
+            if data:
+                file.write(" ".join(map(str, data)))
+            else:
+                file.write("No data")
+
+    def run(self):
+        numbers = self.read_integers()
+
+        if not numbers:
+            print("No valid numbers to process.")
+            return
+
+        even, odd = self.separate_numbers(numbers)
+
+        self.write_file("even.txt", even)
+        self.write_file("odd.txt", odd)
+
+        print("Even and Odd numbers successfully written.")
+        print(f"Processed {len(numbers)} numbers.")
+
+
+if __name__ == "__main__":
+    app = NumberProcessor("numbers.txt")
+    app.run()
