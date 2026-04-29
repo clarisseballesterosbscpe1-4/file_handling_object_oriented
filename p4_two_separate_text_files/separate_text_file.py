071c9503420a97f2a05b41e7ee23ee888b95e150
clarisseballesterosbscpe1-4 <clarisseballesteros08@gmail.com>
2026-04-29 15:39:53 +0800

    open_file

--- p4_two_separate_text_files/separate_text_file.py
+++ p4_two_separate_text_files/separate_text_file.py
@@ -1,3 +1,7 @@
-class integers:
+class NumberProcessor:
     def __init__(self, integers):
         self.integers = integers
+
+    def read_integers(self):
+        with open("integers.txt", "r") as integers_file:
+            numbers =  integers_file.read().split()
